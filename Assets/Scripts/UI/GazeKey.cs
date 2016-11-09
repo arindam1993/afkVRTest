90dedd4cb3f4a15f4182422a6d248245440e456d
Arindam Bose <arindam.1993@gmail.com>
2016-11-09 02:30:49 -0800

    Gaze Keyboard finished

---
+++ Assets/Scripts/UI/GazeKey.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GazeKey : MonoBehaviour {
+
+	// Use this for initialization
+	public void AppendKeyCode(Text nameBox)
+    {
+        nameBox.text += gameObject.name + "\t";
+    }
+}
---
+++ Assets/Scripts/UI/GazeKeyboard.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class ScoreSavedEvent: UnityEvent<string, int>
+{
+}
+
+public class GazeKeyboard : MonoBehaviour {
+
+    public ScoreSavedEvent OnNameSaved;
+
+    public string EnteredText {
+        get
+        {
+            return nameBox.text;
+        }
+    }
+
+    public int Score
+    {
+        get
+        {
+            return Int32.Parse(scoreBox.text);
+        }
+        set
+        {
+            scoreBox.text = value + "";
+        }
+    }
+    Text nameBox;
+    Text scoreBox;
+    Button saveBtn;
+    Button backspaceBtn;
+
+    public int maxNameChars;
+    public int minNameChars;
+    int currNameChars;
+
+	// Use this for initialization
+	void Start () {
+        nameBox = transform.Find("NamePanel").GetComponentInChildren<Text>();
+        scoreBox = transform.Find("ScorePanel").GetComponentInChildren<Text>();
+        saveBtn = transform.Find("SaveBtn").GetComponentInChildren<Button>();
+        backspaceBtn = transform.Find("BackspaceBtn").GetComponentInChildren<Button>();
+        currNameChars = 0;
+        saveBtn.interactable = false;
+
+        OnNameSaved = new ScoreSavedEvent();
+
+        Transform _keys = transform.Find("Keys");
+
+        foreach(Transform _key in _keys)
+        {
+            GazeKey gzKey = _key.gameObject.AddComponent<GazeKey>();
+            Button keyBtn = _key.GetComponent<Button>();
+            keyBtn.onClick.AddListener(() => {
+                if( currNameChars <= maxNameChars)
+                {
+                    gzKey.AppendKeyCode(nameBox);
+                    currNameChars++;
+                }
+
+                if(currNameChars >= minNameChars)
+                {
+                    saveBtn.interactable = true;
+                }
+                
+            });
+        }
+
+        backspaceBtn.onClick.AddListener(() => {
+            if(currNameChars > 0)
+            {
+                //Removing last two characters because of added tabs
+                nameBox.text = nameBox.text.Remove(EnteredText.Length - 2, 2);
+                currNameChars--;
+                if (currNameChars < minNameChars)
+                {
+                    saveBtn.interactable = false;
+                }
+            }
+        });
+
+        saveBtn.onClick.AddListener(() =>{
+            OnNameSaved.Invoke(EnteredText, Score);
+        });
+    }
+}
